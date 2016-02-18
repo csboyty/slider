d1bbe13df89b98b67b353aec99c2613191cfa065
csboyty <csboyty@gmail.com>
2016-02-18 14:51:13 +0800

    init

---
+++ flexslider/js/src/index.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+    $('#slider').flexslider({
+        animation: "slide",
+        controlNav: true,
+        animationLoop: false,
+        slideshow: false,
+        sync: "#carousel"
+    });
+
+});
---
+++ flexslider/js/src/single.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+
+    $('#carousel').flexslider({
+        animation: "slide",
+        controlNav: false,
+        animationLoop: false,
+        slideshow: false,
+        itemWidth: 120,
+        itemMargin: 5,
+        asNavFor: '#slider'
+    });
+
+    $('#slider').flexslider({
+        animation: "slide",
+        controlNav: false,
+        animationLoop: false,
+        slideshow: false,
+        sync: "#carousel"
+    });
+
+});
